28c9eb6efd41da61598bff4210cdc42257049dc1
Dan Griffin <dan.griffin@springernature.com>
2017-01-20 14:52:12 +0000

    Shotgun surgery to make lazyrecords that works with utterlyidle

--- src/com/googlecode/lazyrecords/lucene/NameToLuceneStorageFunction.java
+++
@@ -1,7 +0,0 @@
-package com.googlecode.lazyrecords.lucene;
-
-import java.io.Closeable;
-
-public interface NameToLuceneStorageFunction extends Closeable {
-    LuceneStorage getForName(String name);
-}
